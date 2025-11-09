188dec0927e146347b1009b8de5574b8357c078a
Eunho Ko <eunho.dev@gmail.com>
2025-11-09 13:22:11 +0900

    refactor: remove commented-out code for HTTP request duration histogram

--- src/server.cjs
+++ src/server.cjs
@@ -38,14 +38,6 @@ const httpCounter = new promClient.Counter({
   registers: [register],
 })
 
-// const httpDuration = new promClient.Histogram({
-//   name: 'http_request_duration_seconds',
-//   help: 'Duration of HTTP requests in seconds',
-//   labelNames: ['method', 'route', 'code'],
-//   buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10],
-//   registers: [register],
-// })
-
 // ============================================
 // 미들웨어
 // ============================================
